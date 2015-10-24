90ed0a28b8340fa49e5383d260b66b771d854bc2
Harshul Jain <harshulj@gmail.com>
2015-10-24 14:39:08 +0530

    Added email.

--- tweetlake/config.py
+++ tweetlake/config.py
@@ -1,5 +1,5 @@
 # Twitter Auth Credentials
-consumer_key        = ""
-consumer_secret     = ""
-access_token        = ""
-access_token_secret = ""
+consumer_key        = "2thk9BzFT10uUkwtr68qxyiBB"
+consumer_secret     = "bBn52W8c4E4mQN4dq8UTautewS2oC6QoBw0A93mlNuDPafgrmi"
+access_token        = "1366749896-5VdOmdkk3K9SrcSZNF64CI6lWPod6Zbsbhd6yej"
+access_token_secret = "Tc8RJNsy5iCrPFX74tcBaloW5LZ9vqPWcfG1tof1EI6NW"
