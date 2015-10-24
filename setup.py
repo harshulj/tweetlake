22f8980ccabd3ba53c5b50449a9dd0cf4992b0b2
Harshul Jain <harshulj@gmail.com>
2015-10-24 14:46:53 +0530

    Updated version and setup.py details.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name='Tweet Lake',
-    version='0.1',
-    description='Tweet Lake is a big data project that extracts interesting stats out of tweet corpus.',
+    version='0.1.dev1',
+    description='Tweet Lake is a commandline interface to Twitter Streaming API and big data project that extracts interesting stats out of tweet corpus.',
     url='https://harshulja.in/projects/tweet-lake/',
     author='Harshul Jain',
+    author_email='harshulj@gmail.com',
     license='MIT',
+    keywords='twitter, streaming, api, tweepy, commandline',
     packages=[
         'tweetlake',
         'tweetlake.console',
